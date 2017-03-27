cd3d4872a7685776a418df1cd753977236234d18
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2017-03-27 10:38:44 -0700

    Added a test for missing app token when initializing

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,15 +2,17 @@
 
 class ClientTest extends PHPUnit_Framework_TestCase
 {
-    protected $lifx;
-
-    public function setUp()
+    public function test_it_is_instance_of_lifx_client_object()
     {
-        $this->lifx = new LIFX\Client('NOT_A_REAL_APP_TOKEN');
+        $lifx = new LIFX\Client('NOT_A_REAL_APP_TOKEN');
+
+        $this->assertInstanceOf('LIFX\Client', $lifx);
     }
 
-    public function test_is_instance_of_lifx_client_object()
+    public function test_it_cant_be_initialized_without_an_app_token()
     {
-        $this->assertInstanceOf('LIFX\Client', $this->lifx);
+        $this->setExpectedException('PHPUnit_Framework_Error');
+
+        $lifx = new LIFX\Client();
     }
 }

af1f2b5ff1236f24b564c193ace3c53f8d1cbf05
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2017-03-27 14:25:41 -0700

    Revert "Added a test for missing app token when initializing"
    This reverts commit cd3d4872a7685776a418df1cd753977236234d18.

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,17 +2,15 @@
 
 class ClientTest extends PHPUnit_Framework_TestCase
 {
-    public function test_it_is_instance_of_lifx_client_object()
-    {
-        $lifx = new LIFX\Client('NOT_A_REAL_APP_TOKEN');
+    protected $lifx;
 
-        $this->assertInstanceOf('LIFX\Client', $lifx);
+    public function setUp()
+    {
+        $this->lifx = new LIFX\Client('NOT_A_REAL_APP_TOKEN');
     }
 
-    public function test_it_cant_be_initialized_without_an_app_token()
+    public function test_is_instance_of_lifx_client_object()
     {
-        $this->setExpectedException('PHPUnit_Framework_Error');
-
-        $lifx = new LIFX\Client();
+        $this->assertInstanceOf('LIFX\Client', $this->lifx);
     }
 }
